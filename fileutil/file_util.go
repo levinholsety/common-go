baa3f415908473fda9f01f609c7302bd463ec717
Rui Li <levinholsety@hotmail.com>
2019-12-04 10:11:26 +0800

    Add timeutil.

--- fileutil/file_util.go
+++ fileutil/file_util.go
@@ -5,26 +5,43 @@ import (
 	"os"
 )
 
-// ReadBlocks reads file in blocks.
-func ReadBlocks(filename string, blockSize int, onReadBlock func(block []byte) error) error {
+// OpenRead opens a file for reading.
+func OpenRead(filename string, onOpen func(file *os.File) error) error {
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	buf := make([]byte, blockSize)
-	for true {
-		n, err := file.Read(buf)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if n == 0 && err == io.EOF {
-			break
-		}
-		err = onReadBlock(buf[:n])
-		if err != nil {
-			return err
-		}
+	return onOpen(file)
+}
+
+// OpenWrite opens a file for writing.
+func OpenWrite(filename string, onOpen func(file *os.File) error) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
 	}
-	return nil
+	defer file.Close()
+	return onOpen(file)
+}
+
+// ReadBlocks reads file in blocks.
+func ReadBlocks(filename string, blockSize int, onReadBlock func(block []byte) error) error {
+	return OpenRead(filename, func(file *os.File) error {
+		buf := make([]byte, blockSize)
+		for true {
+			n, err := file.Read(buf)
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if n == 0 && err == io.EOF {
+				break
+			}
+			err = onReadBlock(buf[:n])
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
 }
---
+++ timeutil/time_util.go
@@ -0,0 +1,29 @@
+package timeutil
+
+import (
+	"fmt"
+	"time"
+)
+
+//CurrentTimeMillis returns current time in milliseconds from 1970-01-01 00:00:00 UTC.
+func CurrentTimeMillis() int64 {
+	now := time.Now()
+	return now.Unix()*1000 + int64(now.Nanosecond())/time.Millisecond.Nanoseconds()
+}
+
+// FormatDuration formats time duration.
+func FormatDuration(value time.Duration) string {
+	ms := value.Milliseconds()
+	s := ms / 1000
+	m := s / 60
+	h := m / 60
+	d := h / 24
+	ms = ms % 1000
+	s = s % 60
+	m = m % 60
+	h = h % 24
+	if d > 0 {
+		return fmt.Sprintf("%d-%02d:%02d:%02d.%03d", d, h, m, s, ms)
+	}
+	return fmt.Sprintf("%02d:%02d:%02d.%03d", h, m, s, ms)
+}
