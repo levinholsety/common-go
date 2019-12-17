ea9683566577f7d89446f1926258f0b2267a09f7
Rui Li <levinholsety@hotmail.com>
2019-12-17 14:58:43 +0800

    Optimize commio.BinaryReader.

--- commio/binary_reader.go
+++ commio/binary_reader.go
@@ -2,7 +2,17 @@ package commio
 
 import (
 	"encoding/binary"
+	"errors"
 	"io"
+)
+
+// Delimiters.
+const (
+	DelimNull byte = 0
+)
+
+var (
+	errInvalidSize = errors.New("invalid size")
 )
 
 // NewBinaryReader creates an instance of BinaryReader.
@@ -17,23 +27,11 @@ func NewBinaryReader(r io.Reader, order binary.ByteOrder) *BinaryReader {
 type BinaryReader struct {
 	reader    io.Reader
 	ByteOrder binary.ByteOrder
-	n         int
-}
-
-// Reader returns internal reader.
-func (p *BinaryReader) Reader() io.Reader {
-	return p.reader
-}
-
-// Number returns number of byte has been read.
-func (p *BinaryReader) Number() int {
-	return p.n
 }
 
 // Read reads content into data.
 func (p *BinaryReader) Read(data interface{}) (err error) {
 	err = binary.Read(p.reader, p.ByteOrder, data)
-	p.n += binary.Size(data)
 	return
 }
 
@@ -42,75 +40,175 @@ func (p *BinaryReader) MustRead(data interface{}) {
 	if err := binary.Read(p.reader, p.ByteOrder, data); err != nil {
 		panic(err)
 	}
-	p.n += binary.Size(data)
 	return
 }
 
 // MustReadUInt64 reads a uint64 value.
-func (p *BinaryReader) MustReadUInt64() (v uint64) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadUInt64() (result uint64) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadInt64 reads a int64 value.
-func (p *BinaryReader) MustReadInt64() (v int64) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadInt64() (result int64) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadUInt32 reads a uint32 value.
-func (p *BinaryReader) MustReadUInt32() (v uint32) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadUInt32() (result uint32) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadInt32 reads a int32 value.
-func (p *BinaryReader) MustReadInt32() (v int32) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadInt32() (result int32) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadUInt16 reads a uint16 value.
-func (p *BinaryReader) MustReadUInt16() (v uint16) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadUInt16() (result uint16) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadInt16 reads a int16 value.
-func (p *BinaryReader) MustReadInt16() (v int16) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadInt16() (result int16) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadUInt8 reads a uint8 value.
-func (p *BinaryReader) MustReadUInt8() (v uint8) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadUInt8() (result uint8) {
+	p.MustRead(&result)
 	return
 }
 
 // MustReadInt8 reads a int8 value.
-func (p *BinaryReader) MustReadInt8() (v int8) {
-	p.MustRead(&v)
+func (p *BinaryReader) MustReadInt8() (result int8) {
+	p.MustRead(&result)
 	return
 }
 
-// MustReadByteArray reads byte array in specified size.
-func (p *BinaryReader) MustReadByteArray(size int) (v []byte) {
-	v = make([]byte, size)
-	p.MustRead(&v)
+// MustReadByte reads a byte.
+func (p *BinaryReader) MustReadByte() (result byte) {
+	p.MustRead(&result)
 	return
 }
 
-// MustReadUInt reads byte array in specified size and returns uint.
-func (p *BinaryReader) MustReadUInt(size int) (v uint) {
-	buf := p.MustReadByteArray(size)
-	for i, b := range buf {
-		v |= (uint(b) << ((size - i - 1) * 8))
+// ReadUInt reads byte array in specified size and returns uint.
+func (p *BinaryReader) ReadUInt(size int) (result uint, err error) {
+	switch size {
+	case 0:
+		result = 0
+	case 1:
+		var v uint8
+		if err = p.Read(&v); err != nil {
+			return
+		}
+		result = uint(v)
+	case 2:
+		var v uint16
+		if err = p.Read(&v); err != nil {
+			return
+		}
+		result = uint(v)
+	case 4:
+		var v uint32
+		if err = p.Read(&v); err != nil {
+			return
+		}
+		result = uint(v)
+	case 8:
+		var v uint64
+		if err = p.Read(&v); err != nil {
+			return
+		}
+		result = uint(v)
+	default:
+		err = errInvalidSize
 	}
 	return
 }
 
-// MustReadString reads string in specified size.
-func (p *BinaryReader) MustReadString(size int) (v string) {
-	return string(p.MustReadByteArray(size))
+// MustReadUInt reads byte array in specified size and returns uint.
+func (p *BinaryReader) MustReadUInt(size int) uint {
+	result, err := p.ReadUInt(size)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// ReadByteArray reads byte array in specified size.
+func (p *BinaryReader) ReadByteArray(size int) (result []byte, err error) {
+	result = make([]byte, size)
+	_, err = io.ReadFull(p.reader, result)
+	return
+}
+
+// MustReadByteArray reads byte array in specified size.
+func (p *BinaryReader) MustReadByteArray(size int) []byte {
+	result, err := p.ReadByteArray(size)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// ReadByteArrayUntil reads byte array until delim occurs.
+func (p *BinaryReader) ReadByteArrayUntil(delim byte) (result []byte, err error) {
+	var b byte
+	for err = p.Read(&b); err == nil && b != delim; err = p.Read(&b) {
+		result = append(result, b)
+	}
+	return
+}
+
+// MustReadByteArrayUntil reads byte array until delim occurs.
+func (p *BinaryReader) MustReadByteArrayUntil(delim byte) []byte {
+	result, err := p.ReadByteArrayUntil(delim)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// ReadStringFixed reads string in specified size.
+func (p *BinaryReader) ReadStringFixed(size int) (result string, err error) {
+	buf, err := p.ReadByteArray(size)
+	if err != nil {
+		return
+	}
+	result = string(buf)
+	return
+}
+
+// MustReadStringFixed reads string in specified size.
+func (p *BinaryReader) MustReadStringFixed(size int) string {
+	result, err := p.ReadStringFixed(size)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// ReadString reads string until null character occurs.
+func (p *BinaryReader) ReadString() (result string, err error) {
+	buf, err := p.ReadByteArrayUntil(DelimNull)
+	if err != nil {
+		return
+	}
+	result = string(buf)
+	return
+}
+
+// MustReadString reads string until null character occurs.
+func (p *BinaryReader) MustReadString() string {
+	result, err := p.ReadString()
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
