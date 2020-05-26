4e25b3982233da638d4691e8d793406e2d3d19d1
Rui Li <levinholsety@hotmail.com>
2020-05-26 09:27:34 +0800

    Fix the bug of invalid env in command.

--- utils/command.go
+++ utils/command.go
@@ -8,36 +8,37 @@ import (
 
 // NewCommand creates a Command.
 func NewCommand(path string) *Command {
-	return (&Command{Path: path}).ResetEnvVars()
+	return (&Command{path: path}).ResetEnvVars()
 }
 
 // Command provides functions to execute command.
 type Command struct {
-	Path                 string
-	EnvironmentVariables []string
+	path string
+	env  []string
 }
 
 // AddEnvVar adds a environment variable to current command.
 func (p *Command) AddEnvVar(name, value string) *Command {
-	p.EnvironmentVariables = append(p.EnvironmentVariables, fmt.Sprintf("%s=%s", name, value))
+	p.env = append(p.env, fmt.Sprintf("%s=%s", name, value))
 	return p
 }
 
 // ResetEnvVars resets environment variables of current command to system defaults.
 func (p *Command) ResetEnvVars() *Command {
-	p.EnvironmentVariables = os.Environ()
+	p.env = os.Environ()
 	return p
 }
 
 // ClearEnvVars clears environment variables of current command.
 func (p *Command) ClearEnvVars() *Command {
-	p.EnvironmentVariables = []string{}
+	p.env = []string{}
 	return p
 }
 
 // Execute executes command and redirect command output and error to standard output and error.
 func (p *Command) Execute(args ...string) error {
-	cmd := exec.Command(p.Path, args...)
+	cmd := exec.Command(p.path, args...)
+	cmd.Env = p.env
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
@@ -45,5 +46,7 @@ func (p *Command) Execute(args ...string) error {
 
 // ExecuteOutput executes command and returns output combined with command output and error.
 func (p *Command) ExecuteOutput(args ...string) ([]byte, error) {
-	return exec.Command(p.Path, args...).Output()
+	cmd := exec.Command(p.path, args...)
+	cmd.Env = p.env
+	return cmd.Output()
 }
