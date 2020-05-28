b12b6b8d104da4dbac65a409bdb4cad375e22573
Rui Li <levinholsety@hotmail.com>
2020-05-28 09:21:28 +0800

    Optimize function comm.FormatDuration.

--- comm/time_util.go
+++ comm/time_util.go
@@ -15,19 +15,24 @@ func CurrentTimeMillis() int64 {
 	return TimeMillis(time.Now())
 }
 
-// FormatDuration formats time duration.
-func FormatDuration(value time.Duration) string {
-	ms := value.Nanoseconds() / 1e6
-	s := ms / 1000
-	m := s / 60
-	h := m / 60
-	d := h / 24
-	ms = ms % 1000
-	s = s % 60
-	m = m % 60
-	h = h % 24
+func div(a, b int64) (int64, int64) {
+	return a / b, a % b
+}
+
+const (
+	timeDurationFormat         = "%02d:%02d:%02d.%03d"
+	timeDurationWithDaysFormat = "%dd " + timeDurationFormat
+)
+
+// FormatTimeDuration formats time duration.
+func FormatTimeDuration(value time.Duration) string {
+	ms := value.Nanoseconds() / int64(time.Millisecond)
+	s, ms := div(ms, 1000)
+	m, s := div(s, 60)
+	h, m := div(m, 60)
+	d, h := div(h, 24)
 	if d > 0 {
-		return fmt.Sprintf("%dd %02d:%02d:%02d.%03d", d, h, m, s, ms)
+		return fmt.Sprintf(timeDurationWithDaysFormat, d, h, m, s, ms)
 	}
-	return fmt.Sprintf("%02d:%02d:%02d.%03d", h, m, s, ms)
+	return fmt.Sprintf(timeDurationFormat, h, m, s, ms)
 }
