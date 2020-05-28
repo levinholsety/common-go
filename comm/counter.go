a6f3c4516acaf951ffa73b047c22a8c9c90831a5
Rui Li <levinholsety@hotmail.com>
2020-05-28 10:01:24 +0800

    Add function N() to comm.Counter.

--- comm/counter.go
+++ comm/counter.go
@@ -2,6 +2,7 @@ package comm
 
 // Counter provides simple methods for counting.
 type Counter struct {
+	n     int
 	count int
 }
 
@@ -10,8 +11,14 @@ func (p *Counter) Count() int {
 	return p.count
 }
 
-// Add adds number to total count and returns the input error.
+// N returns the latest number added to counter.
+func (p *Counter) N() int {
+	return p.n
+}
+
+// Add adds number to counter and returns the input error.
 func (p *Counter) Add(n int, err error) error {
+	p.n = n
 	p.count += n
 	return err
 }
